dccde2afb68e6207b2d7873a610769e662e67ee4
Martin Welss <martin.welss@iais.fraunhofer.de>
2022-08-10 17:18:07 +0200

    set worker timeout to 300 sec. for deployments

--- scripts/start_playground.sh
+++ scripts/start_playground.sh
@@ -4,4 +4,4 @@ cd /home/ai4eu
 cd playground-app/
 export SECRET_KEY=$(uuidgen)
 # nohup python3 https_app.py > playground.log 2>&1 &
-gunicorn -w 20 -b 0.0.0.0:443 --keyfile /etc/letsencrypt/live/playground.ki-lab.nrw/privkey.pem --certfile  /etc/letsencrypt/live/playground.ki-lab.nrw/fullchain.pem  gunicorn_app:app
+gunicorn -w 20 -b 0.0.0.0:443 --timeout 300 --keyfile /etc/letsencrypt/live/playground.ki-lab.nrw/privkey.pem --certfile  /etc/letsencrypt/live/playground.ki-lab.nrw/fullchain.pem  gunicorn_app:app
