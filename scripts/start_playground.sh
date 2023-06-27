015a8a41d13274d616f8adacdc6e70a1515ef3c5
Martin Welss <martin.welss@iais.fraunhofer.de>
2023-06-27 23:02:46 +0200

    #26 fix for gunicorn run

--- scripts/start_playground.sh
+++ scripts/start_playground.sh
@@ -3,7 +3,6 @@ cd /home/ai4eu
 . grpc/bin/activate
 cd playground-app/
 export SECRET_KEY=$(uuidgen)
-# nohup python3 https_app.py > playground.log 2>&1 &
 HTTPS_CHAIN=$(jq -r '.https_chain' config.json)
 HTTPS_KEY=$(jq -r '.https_key' config.json)
-gunicorn -w 20 -b 0.0.0.0:443 --timeout 300 --keyfile $HTTPS_KEY --certfile $HTTPS_CHAIN  gunicorn_app:app
+gunicorn -w 20 -b 0.0.0.0:443 --timeout 300 --keyfile $HTTPS_KEY --certfile $HTTPS_CHAIN  app:app
